b25c4c4c46501faea1f32b82a2ba0de0b1f07fd9
ezzpharmacy129-blip <ezzpharmacy129@gmail.com>
2026-08-29 01:35:19 +0300

    Wire visible Excel export into Flask app

--- sitecustomize.py
+++ sitecustomize.py
@@ -12,12 +12,14 @@ def _protected_init(self, *args, **kwargs):
     from admin_state_controls import install_admin_state_controls
     from pending_availability_fix import install_pending_availability_fix
     from data_export import install_data_export
+    from export_ui import install_export_ui
     from db import db
     install_auth(self, db)
     install_security_extensions(self, db)
     install_admin_state_controls(self, db)
     install_pending_availability_fix(db)
     install_data_export(self, db)
+    install_export_ui(self)
 
 
 if not getattr(flask.Flask, "_ezz_auth_constructor_patched_v2", False):
