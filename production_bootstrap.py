27c5f472870219c4f05e51719a88e075d57b32e5
ezzpharmacy129-blip <ezzpharmacy129@gmail.com>
2026-09-07 06:32:48 +0300

    chore(debug): enable temporary non-secret login diagnostics

--- production_bootstrap.py
+++ production_bootstrap.py
@@ -17,11 +17,13 @@ def install_production_security(app, db):
     from authorization_policy import install_authorization
     from auth_security_extensions import install_security_extensions
     from admin_recovery import install_admin_recovery
+    from login_diagnostics import install_login_diagnostics
 
     install_auth(app, db)
     install_authorization(app)
     install_security_extensions(app, db)
     install_admin_recovery(app, db)
+    install_login_diagnostics(app, db)
     app._ezz_production_security_bootstrapped = True
     app.extensions["ezz_production_security"] = {"installed": True, "cloud": True}
     return True
